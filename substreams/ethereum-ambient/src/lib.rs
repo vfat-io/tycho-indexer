40745c7788cb5726011c1214805b9e4569a83c1f
smart_monkey <rouven@propellerheads.xyz>
2023-12-11 16:24:37 +0100

    map_changes: Adjust TransactionChanges construction

--- substreams/ethereum-ambient/src/lib.rs
+++ substreams/ethereum-ambient/src/lib.rs
@@ -70,7 +70,12 @@ fn map_changes(
 ) -> Result<tycho::BlockContractChanges, substreams::errors::Error> {
     let mut block_changes = tycho::BlockContractChanges { block: None, changes: Vec::new() };
 
-    let mut tx_change = tycho::TransactionChanges { tx: None, contract_changes: Vec::new() };
+    let mut tx_change = tycho::TransactionChanges {
+        tx: None,
+        contract_changes: Vec::new(),
+        components: Vec::new(),
+        tvl: Vec::new(),
+    };
 
     let mut changed_contracts: HashMap<Vec<u8>, InterimContractChange> = HashMap::new();
 
