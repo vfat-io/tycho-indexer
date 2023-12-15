b2dfe7eac9df01164d3d42ca710c0da68220bbb0
Louise Poole <louise@datarevenue.com>
2023-12-15 13:08:35 +0200

    Update appropriate msg field types to String

--- tycho-indexer/src/extractor/evm/mod.rs
+++ tycho-indexer/src/extractor/evm/mod.rs
@@ -1,13 +1,10 @@
 #![allow(dead_code)]
+
 use std::{
     collections::{hash_map::Entry, HashMap},
     ops::Deref,
 };
 
-use crate::{
-    models::{ProtocolSystem, ProtocolType},
-    pb::tycho::evm::v1 as substreams,
-};
 use chrono::NaiveDateTime;
 use ethers::{
     types::{H160, H256, U256},
@@ -20,7 +17,8 @@ use utils::{pad_and_parse_32bytes, pad_and_parse_h160};
 
 use crate::{
     hex_bytes::Bytes,
-    models::{Chain, ExtractorIdentity, NormalisedMessage},
+    models::{Chain, ExtractorIdentity, NormalisedMessage, ProtocolSystem, ProtocolType},
+    pb::tycho::evm::v1 as substreams,
     storage::{ChangeType, StateGatewayType},
 };
 
@@ -403,6 +401,7 @@ impl AccountUpdateWithTx {
         Ok(update)
     }
 }
+
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct TvlChange {
     token: H160,
@@ -478,10 +477,7 @@ impl ProtocolComponent {
         protocol_type: ProtocolType,
         chain: Chain,
     ) -> Result<Self, ExtractionError> {
-        let id = ContractId(
-            String::from_utf8(msg.id)
-                .map_err(|error| ExtractionError::DecodeError(error.to_string()))?,
-        );
+        let id = ContractId(msg.id);
 
         let tokens = msg
             .tokens
@@ -498,23 +494,11 @@ impl ProtocolComponent {
             .map(ContractId)
             .collect::<Vec<_>>();
 
-        let keys = msg
-            .static_att
-            .clone()
-            .into_iter()
-            .map(|attr| {
-                String::from_utf8(attr.name)
-                    .map_err(|error| ExtractionError::DecodeError(error.to_string()))
-            })
-            .collect::<Result<Vec<String>, _>>()?;
-
-        let values: Vec<_> = msg
+        let static_attributes = msg
             .static_att
             .into_iter()
-            .map(|attr| Bytes::from(attr.value))
-            .collect();
-
-        let attribute_map: HashMap<_, _> = keys.into_iter().zip(values).collect();
+            .map(|attribute| Ok((attribute.name, Bytes::from(attribute.value))))
+            .collect::<Result<HashMap<_, _>, ExtractionError>>()?;
 
         Ok(Self {
             id,
@@ -522,7 +506,7 @@ impl ProtocolComponent {
             protocol_system,
             tokens,
             contract_ids,
-            static_attributes: attribute_map,
+            static_attributes,
             chain,
         })
     }
@@ -638,20 +622,13 @@ impl ProtocolState {
         msg: substreams::StateChanges,
         tx: &Transaction,
     ) -> Result<Self, ExtractionError> {
-        let component_id = String::from_utf8(msg.component_id)
-            .map_err(|err| ExtractionError::DecodeError(err.to_string()))?;
-
         let attributes = msg
             .attributes
             .into_iter()
-            .map(|attribute| {
-                let name = String::from_utf8(attribute.name)
-                    .map_err(|err| ExtractionError::DecodeError(err.to_string()))?;
-                Ok((name, Bytes::from(attribute.value)))
-            })
+            .map(|attribute| Ok((attribute.name, Bytes::from(attribute.value))))
             .collect::<Result<HashMap<_, _>, ExtractionError>>()?;
 
-        Ok(Self { component_id, attributes, modify_tx: *tx })
+        Ok(Self { component_id: msg.component_id, attributes, modify_tx: *tx })
     }
 
     /// Merges this update with another one.
@@ -791,9 +768,10 @@ impl BlockEntityChanges {
 
 #[cfg(test)]
 pub mod fixtures {
-    use super::*;
     use ethers::abi::AbiEncode;
     use prost::Message;
+
+    use super::*;
 
     pub const HASH_256_0: &str =
         "0x0000000000000000000000000000000000000000000000000000000000000000";
@@ -896,10 +874,7 @@ pub mod fixtures {
                     },
                 ],
                 components: vec![ProtocolComponent {
-                    id: hex::decode(
-                        "0xaaaaaaaaa24eeeb8d57d431224f73832bc34f688".trim_start_matches("0x"),
-                    )
-                    .unwrap(),
+                    id: "0xaaaaaaaaa24eeeb8d57d431224f73832bc34f688".to_owned(),
                     tokens: vec![
                         hex::decode(
                             "0xaaaaaaaaa24eeeb8d57d431224f73832bc34f688".trim_start_matches("0x"),
@@ -915,8 +890,8 @@ pub mod fixtures {
                         "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".to_string(),
                     ],
                     static_att: vec![
-                        Attribute { name: b"key1".to_vec(), value: b"value1".to_vec() },
-                        Attribute { name: b"key2".to_vec(), value: b"value2".to_vec() },
+                        Attribute { name: "key1".to_owned(), value: b"value1".to_vec() },
+                        Attribute { name: "key2".to_owned(), value: b"value2".to_vec() },
                     ],
                 }],
                 tvl: vec![BalanceChange {
@@ -933,25 +908,19 @@ pub mod fixtures {
 
     pub fn pb_state_changes() -> crate::pb::tycho::evm::v1::StateChanges {
         use crate::pb::tycho::evm::v1::*;
-        let id = "test".to_owned().into_bytes();
-        let res1_name = "reserve1".to_owned().into_bytes();
-        let res2_name = "reserve2".to_owned().into_bytes();
         let res1_value = 1000_u64.to_be_bytes().to_vec();
         let res2_value = 500_u64.to_be_bytes().to_vec();
         StateChanges {
-            component_id: id,
+            component_id: "test".to_owned(),
             attributes: vec![
-                Attribute { name: res1_name, value: res1_value },
-                Attribute { name: res2_name, value: res2_value },
+                Attribute { name: "reserve1".to_owned(), value: res1_value },
+                Attribute { name: "reserve2".to_owned(), value: res2_value },
             ],
         }
     }
 
     pub fn pb_block_entity_changes() -> crate::pb::tycho::evm::v1::BlockEntityChanges {
         use crate::pb::tycho::evm::v1::*;
-        let id = "test1".to_owned().into_bytes();
-        let res1_name = "reserve1".to_owned().into_bytes();
-        let res2_name = "reserve2".to_owned().into_bytes();
         let res1_value = 1000_u64.to_be_bytes().to_vec();
         let res2_value = 500_u64.to_be_bytes().to_vec();
         BlockEntityChanges {
@@ -971,12 +940,18 @@ pub mod fixtures {
                 }),
                 state_changes: vec![
                     StateChanges {
-                        component_id: id.clone(),
-                        attributes: vec![Attribute { name: res1_name, value: res1_value }],
+                        component_id: "test1".to_owned(),
+                        attributes: vec![Attribute {
+                            name: "reserve1".to_owned(),
+                            value: res1_value,
+                        }],
                     },
                     StateChanges {
-                        component_id: id,
-                        attributes: vec![Attribute { name: res2_name, value: res2_value }],
+                        component_id: "test1".to_owned(),
+                        attributes: vec![Attribute {
+                            name: "reserve2".to_owned(),
+                            value: res2_value,
+                        }],
                     },
                 ],
             }],
@@ -986,17 +961,17 @@ pub mod fixtures {
 
 #[cfg(test)]
 mod test {
+    use std::str::FromStr;
 
-    use crate::extractor::evm::fixtures::transaction01;
-
-    use super::*;
-    use crate::{
-        models::{FinancialType, ImplementationType, ProtocolSystem, ProtocolType},
-        pb::tycho::evm::v1::Attribute,
-    };
     use actix_web::body::MessageBody;
     use rstest::rstest;
-    use std::str::FromStr;
+
+    use crate::{
+        extractor::evm::fixtures::transaction01,
+        models::{FinancialType, ImplementationType},
+    };
+
+    use super::*;
 
     const HASH_256_0: &str = "0x0000000000000000000000000000000000000000000000000000000000000000";
     const HASH_256_1: &str = "0x0000000000000000000000000000000000000000000000000000000000000001";
@@ -1470,7 +1445,6 @@ mod test {
             attributes: expected_attributes,
             modify_tx: new_tx,
         }];
-        dbg!(&expected_result);
 
         let res = block_changes
             .aggregate_updates()
@@ -1503,14 +1477,14 @@ mod test {
 
         // Sample data for testing
         let static_att = vec![
-            Attribute { name: balance_key.as_bytes().to_vec(), value: balance_value.to_vec() },
-            Attribute {
-                name: factory_address_key.as_bytes().to_vec(),
+            substreams::Attribute { name: balance_key.to_owned(), value: balance_value.to_vec() },
+            substreams::Attribute {
+                name: factory_address_key.to_owned(),
                 value: factory_address.to_vec(),
             },
         ];
         let msg = substreams::ProtocolComponent {
-            id: b"component_id".to_vec(),
+            id: "component_id".to_owned(),
             tokens: vec![b"token1".to_vec(), b"token2".to_vec()],
             contracts: vec!["contract1".to_string(), "contract2".to_string()],
             static_att,
--- tycho-indexer/src/pb/sf.substreams.rs
+++ tycho-indexer/src/pb/sf.substreams.rs
@@ -2,7 +2,7 @@
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct FieldOptions {
-    /// this option informs the `substreams pack` command that it should treat the corresponding manifest value as a path to a file, putting its content as bytes in this field. 
+    /// this option informs the `substreams pack` command that it should treat the corresponding manifest value as a path to a file, putting its content as bytes in this field.
     /// must be applied to a `bytes` or `string` field
     #[prost(bool, tag="1")]
     pub load_from_file: bool,
