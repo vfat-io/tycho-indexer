571528d291b16ebc1815e6df4001e002663381e0
smart_monkey <rouven@propellerheads.xyz>
2024-01-05 16:29:19 +0100

    Adjust test assets

--- tycho-indexer/src/extractor/evm/mod.rs
+++ tycho-indexer/src/extractor/evm/mod.rs
@@ -1063,7 +1063,12 @@ pub mod fixtures {
                         },
                     ],
                     change: ChangeType::Creation.into(),
-                    protocol_type: None, // TODO
+                    protocol_type: Some(ProtocolType {
+                        name: "Ambient".to_string(),
+                        financial_type: 0,
+                        attribute_schema: vec![],
+                        implementation_type: 0,
+                    }),
                 }],
                 balance_changes: vec![BalanceChange {
                     token: hex::decode(
@@ -1171,7 +1176,12 @@ pub mod fixtures {
                             change: ChangeType::Creation.into(),
                         }],
                         change: ChangeType::Creation.into(),
-                        protocol_type: None, //TODO
+                        protocol_type: Some(ProtocolType {
+                            name: "Ambient".to_string(),
+                            financial_type: 0,
+                            attribute_schema: vec![],
+                            implementation_type: 0,
+                        }),
                     }],
                     balance_changes: vec![],
                 },
@@ -1241,7 +1251,12 @@ pub mod fixtures {
                 },
             ],
             change: ChangeType::Creation.into(),
-            protocol_type: None, //TODO
+            protocol_type: Some(ProtocolType {
+                name: "Ambient".to_string(),
+                financial_type: 0,
+                attribute_schema: vec![],
+                implementation_type: 0,
+            }),
         }
     }
 }
