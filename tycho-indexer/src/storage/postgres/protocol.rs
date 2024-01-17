71aa5785a5db834bc3b537f5bd964391d1a2d3f1
smart_monkey <rouven@propellerheads.xyz>
2024-01-17 13:19:14 +0100

    Draft: upsert_components

--- tycho-indexer/src/storage/postgres/protocol.rs
+++ tycho-indexer/src/storage/postgres/protocol.rs
@@ -1,6 +1,7 @@
 #![allow(unused_variables)]
 
 use async_trait::async_trait;
+use std::collections::HashMap;
 
 use diesel::prelude::*;
 use diesel_async::{AsyncPgConnection, RunQueryDsl};
@@ -41,25 +42,42 @@ where
 
     async fn upsert_components(
         &self,
-        new: &[ProtocolComponent],
-        chain_id: i64,
+        new: &[&Self::ProtocolComponent],
         conn: &mut Self::DB,
     ) -> Result<(), StorageError> {
         use super::schema::protocol_component::dsl::*;
+        let mut values: Vec<orm::NewProtocolComponent> = vec![];
+        //let values: Vec<NewProtocolComponent> = new
+        //    .into_iter()
+        //    .map(|pc| async {
+        //        pc.to_storage(
+        //            self.get_chain_id(&pc.chain),
+        //            self._get_or_create_protocol_system_id(pc.protocol_system, conn)
+        //                .await?,
+        //           Default::default(),
+        //      )
+        //  })
+        //  .collect::<Vec<_>>();
 
-        let values = new
-            .into_iter()
-            .map(|pc| pc.to_storage())
-            .collect();
-
+        println!("a");
+        for pc in new {
+            let new_pc = pc
+                .to_storage(self.get_chain_id(&pc.chain), 0, Default::default())
+                .unwrap();
+            values.push(new_pc);
+        }
+        println!("b");
         diesel::insert_into(protocol_component)
             .values(&values)
-            .on_conflict(external_id)
+            .on_conflict((chain_id, protocol_system_id, external_id))
             .do_update()
-            .set(&values)
+            .set(values.get(0).unwrap())
             .execute(conn)
             .await
-            .map_err(|err| StorageError::from_diesel(err, "ProtocolType", &values.name, None))?;
+            .map_err(|err| StorageError::from_diesel(err, "ProtocolComponent", "", None))
+            .unwrap();
+
+        Ok(())
     }
 
     async fn get_states(
@@ -142,8 +160,12 @@ where
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::extractor::evm;
+    use crate::{
+        extractor::{evm, evm::ContractId},
+        storage::ChangeType,
+    };
     use diesel_async::AsyncConnection;
+    use std::collections::HashMap;
 
     type EVMGateway = PostgresGateway<
         evm::Block,
@@ -181,5 +203,57 @@ mod test {
             .await
             .unwrap();
         assert_eq!(protocol_system_id, 1);
+    }
+
+    #[tokio::test]
+    async fn test_upsert_components() {
+        let mut conn = setup_db().await;
+
+        let gw = EVMGateway::from_connection(&mut conn).await;
+        gw.chain_id_cache
+            .map_enum
+            .lock()
+            .unwrap()
+            .insert(1, Chain::Ethereum);
+
+        gw.chain_id_cache
+            .map_id
+            .lock()
+            .unwrap()
+            .insert(Chain::Ethereum, 1);
+        // Define test data
+        let protocol_system = ProtocolSystem::default(); // Replace with actual test data
+        let chain = Chain::default(); // Replace with actual test data
+        let new_component = ProtocolComponent {
+            id: ContractId("test_contract_id".to_string()),
+            protocol_system: protocol_system.clone(),
+            protocol_type_id: "1".to_string(),
+            chain: chain.clone(),
+            tokens: vec![],
+            contract_ids: vec![],
+            static_attributes: HashMap::new(),
+            change: ChangeType::Creation,
+        };
+
+        // Call the function under test
+        let result = gw
+            .upsert_components(&[&new_component.clone()], &mut conn)
+            .await;
+
+        // Assert the result
+        assert!(result.is_ok());
+
+        // Optionally, you can query the database to verify the inserted data
+        use crate::storage::postgres::schema::protocol_component::dsl::*;
+        //let inserted_data = protocol_component
+        //    .filter(external_id.eq("test_contract_id"))
+        //    .first::<ProtocolComponent>(&mut conn)
+        //    .optional()
+        //    .await;
+
+        // Assert that the data was inserted as expected
+        //assert!(inserted_data.is_ok());
+        //let inserted_data = inserted_data.unwrap();
+        //assert_eq!(Some(new_component), inserted_data);
     }
 }
