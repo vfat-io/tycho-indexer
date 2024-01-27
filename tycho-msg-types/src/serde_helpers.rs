013f1faa5836bd49cf117fcd0660eefe6b2c7e24
kayibal <alan@datarevenue.com>
2024-01-26 21:32:41 -0300

    Add tycho client library

--- tycho-msg-types/src/serde_helpers.rs
+++ tycho-msg-types/src/serde_helpers.rs
@@ -1,6 +1,23 @@
+use hex::FromHexError;
+
+fn decode_hex_with_prefix(val: &str) -> Result<Vec<u8>, FromHexError> {
+    let mut stripped: String =
+        if let Some(stripped) = val.strip_prefix("0x") { stripped } else { val }.into();
+
+    // Check if the length of the string is odd
+    if stripped.len() % 2 != 0 {
+        // If it's odd, prepend a zero
+        stripped.insert(0, '0');
+    }
+
+    hex::decode(&stripped)
+}
+
 /// serde functions for handling bytes as hex strings, such as [bytes::Bytes]
 pub mod hex_bytes {
     use serde::{Deserialize, Deserializer, Serializer};
+
+    use super::decode_hex_with_prefix;
 
     /// Serialize a byte vec as a hex string with 0x prefix
     pub fn serialize<S, T>(x: T, s: S) -> Result<S::Ok, S::Error>
@@ -19,18 +36,15 @@ pub mod hex_bytes {
         T: From<Vec<u8>>,
     {
         let value = String::deserialize(d)?;
-        if let Some(value) = value.strip_prefix("0x") {
-            hex::decode(value)
-        } else {
-            hex::decode(&value)
-        }
-        .map(Into::into)
-        .map_err(|e| serde::de::Error::custom(e.to_string()))
+        decode_hex_with_prefix(&value)
+            .map(Into::into)
+            .map_err(|e| serde::de::Error::custom(e.to_string()))
     }
 }
 
 /// serde functions for handling Option of bytes
 pub mod hex_bytes_option {
+    use super::decode_hex_with_prefix;
     use serde::{Deserialize, Deserializer, Serializer};
 
     /// Serialize a byte vec as a Some hex string with 0x prefix
@@ -56,19 +70,17 @@ pub mod hex_bytes_option {
         let value: Option<String> = Option::deserialize(d)?;
 
         match value {
-            Some(val) => {
-                let val = if let Some(stripped) = val.strip_prefix("0x") { stripped } else { &val };
-                hex::decode(val)
-                    .map(Into::into)
-                    .map(Some)
-                    .map_err(|e| serde::de::Error::custom(e.to_string()))
-            }
+            Some(val) => decode_hex_with_prefix(&val)
+                .map(Into::into)
+                .map(Some)
+                .map_err(|e| serde::de::Error::custom(e.to_string())),
             None => Ok(None),
         }
     }
 }
 
 pub mod hex_hashmap_key {
+    use super::decode_hex_with_prefix;
     use std::collections::HashMap;
 
     use serde::{de, ser::SerializeMap, Deserialize, Deserializer, Serialize, Serializer};
@@ -92,10 +104,11 @@ pub mod hex_hashmap_key {
         V: Deserialize<'de>,
     {
         let interim = HashMap::<String, V>::deserialize(d)?;
+
         interim
             .into_iter()
             .map(|(k, v)| {
-                let k = hex::decode(k).map_err(|e| de::Error::custom(e.to_string()))?;
+                let k = decode_hex_with_prefix(&k).map_err(|e| de::Error::custom(e.to_string()))?;
                 return Ok((k, v))
             })
             .collect::<Result<HashMap<_, _>, _>>()
@@ -106,6 +119,8 @@ pub mod hex_hashmap_key_value {
     use std::collections::HashMap;
 
     use serde::{de, ser::SerializeMap, Deserialize, Deserializer, Serializer};
+
+    use super::decode_hex_with_prefix;
 
     pub fn serialize<S>(x: &HashMap<Vec<u8>, Vec<u8>>, s: S) -> Result<S::Ok, S::Error>
     where
@@ -129,8 +144,8 @@ pub mod hex_hashmap_key_value {
         interim
             .into_iter()
             .map(|(k, v)| {
-                let k = hex::decode(k).map_err(|e| de::Error::custom(e.to_string()))?;
-                let v = hex::decode(v).map_err(|e| de::Error::custom(e.to_string()))?;
+                let k = decode_hex_with_prefix(&k).map_err(|e| de::Error::custom(e.to_string()))?;
+                let v = decode_hex_with_prefix(&v).map_err(|e| de::Error::custom(e.to_string()))?;
                 return Ok((k, v))
             })
             .collect::<Result<HashMap<_, _>, _>>()
