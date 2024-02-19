014a47ffddaba8fdb057afcf50bea498eb37ed52
kayibal <alan@datarevenue.com>
2024-02-19 19:18:58 +0000

    Format sql

--- tycho-indexer/migrations/2024-02-16-203155_v0.2.1/down.sql
+++ tycho-indexer/migrations/2024-02-16-203155_v0.2.1/down.sql
@@ -6,3 +6,4 @@ DROP INDEX IF EXISTS idx_protocol_state_component_id_attribute_name_valid_to;
 DROP INDEX IF EXISTS idx_component_balance_component_id_token_id_valid_to;
 
 DROP INDEX IF EXISTS idx_account_address;
+
