5e3385d12889b96642cdd17d8137e84d09dc7021
tvinagre <thales@datarevenue.com>
2024-03-14 22:29:40 +0100

    Add release steps to Github CI (#189)
    * Add release steps to Github CI

--- stable-build.sh
+++ stable-build.sh
@@ -6,6 +6,7 @@ set -e
 SUBSTREAM_PKGS=$(cargo ws list | grep -i substreams | grep -v -e substreams-common -e substreams-helper)
 WORKDIR=$(pwd)
 TYCHO_INDEXER=tycho-indexer
+TYCHO_CLIENT=tycho-client
 
 for PACKAGE in $SUBSTREAM_PKGS
 do
@@ -19,3 +20,6 @@ done
 
 echo "Building tycho: $TYCHO_INDEXER"
 cargo build --package $TYCHO_INDEXER --release
+
+echo "Building tycho-client: $TYCHO_CLIENT}"
+cargo build --package $TYCHO_CLIENT --release
