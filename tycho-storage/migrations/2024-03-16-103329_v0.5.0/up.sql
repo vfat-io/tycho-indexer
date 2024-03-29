939652436afeaf4c2a540ad8962753ba7ebb16f3
kayibal <alan@datarevenue.com>
2024-03-29 12:33:14 +0000

    Adjust migration after test on bigger dump.
    Main change is that we now migrate latest entries ourselves, instead of using partition_data_proc. Reason being that the procedure would create a partition for our placeholder date in the year 262142. This is obviously not the desired behaviour, so we move these rows first, then afterward migrate the remaining data in the old table.
    
    Reduced the migration period from 90 to 7d and instead we keep the old table around. This allows the migration to run quicker, reducing downtimes. If we need more historical data it can be accomplished by simply re-executing partition_data_proc.

--- tycho-storage/migrations/2024-03-16-103329_v0.5.0/up.sql
+++ tycho-storage/migrations/2024-03-16-103329_v0.5.0/up.sql
@@ -1,5 +1,6 @@
 -- https://github.com/pgpartman/pg_partman/blob/master/doc/pg_partman.md
 CREATE SCHEMA partman;
+
 CREATE EXTENSION pg_partman WITH SCHEMA partman;
 
 -- remove audit table to speed up inserts
@@ -39,18 +40,24 @@ DROP TRIGGER IF EXISTS audit_table_token ON token;
 
 DROP TRIGGER IF EXISTS audit_table_token_price ON token_price;
 
-DROP TRIGGER IF EXISTS audit_table_transaction ON transaction;
+DROP TRIGGER IF EXISTS audit_table_transaction ON TRANSACTION;
 
 DROP TRIGGER IF EXISTS audit_table_protocol_component ON protocol_component;
 
-DROP TRIGGER IF EXISTS  invalidate_previous_entry_protocol_calls_contract ON protocol_calls_contract;
+DROP TRIGGER IF EXISTS invalidate_previous_entry_protocol_calls_contract ON protocol_calls_contract;
 
 -- PROTOCOL STATE
 ALTER TABLE protocol_state RENAME TO protocol_state_old;
 
-UPDATE protocol_state_old SET valid_to = '262142-12-31T23:59:59.9999Z' WHERE valid_to IS NULL;
+UPDATE
+    protocol_state_old
+SET
+    valid_to = '262142-12-31T23:59:59.9999Z'
+WHERE
+    valid_to IS NULL;
 
-ALTER TABLE protocol_state_old ALTER COLUMN valid_to SET NOT NULL;
+ALTER TABLE protocol_state_old
+    ALTER COLUMN valid_to SET NOT NULL;
 
 ALTER INDEX idx_protocol_state_component_id_attribute_name_valid_to RENAME TO idx_protocol_state_component_id_attribute_name_valid_to_old;
 
@@ -84,33 +91,59 @@ ALTER TABLE public.protocol_state
     ADD CONSTRAINT protocol_state_protocol_component_id_fkey FOREIGN KEY (protocol_component_id) REFERENCES protocol_component(id);
 
 SELECT
-    partman.create_parent(
-            p_parent_table := 'public.protocol_state',
-            p_control := 'valid_to',
-            p_interval := '1 day',
-            p_type := 'range',
-            p_premake := 7,
-            p_default_table := TRUE,
-            p_automatic_maintenance := 'on'
-    );
+    partman.create_parent(p_parent_table := 'public.protocol_state', p_control := 'valid_to', p_interval := '1 day', p_type := 'range', p_premake := 7, p_default_table := TRUE, p_automatic_maintenance := 'on');
 
-CALL partman.partition_data_proc(p_parent_table := 'public.protocol_state', p_loop_count := 90, p_source_table := 'public.protocol_state_old', p_order := 'DESC');
+-- Move latest values directly into the parent table - they should end up in default
+-- without this statement we would create a 262142 partition when moving data with
+-- `partition_data_proc` below.
+WITH latest_protocol_state AS (
+    DELETE FROM protocol_state_old
+    WHERE valid_to = '262142-12-31T23:59:59.9999Z'
+    RETURNING
+        *)
+INSERT INTO protocol_state
+SELECT
+    attribute_name,
+    attribute_value,
+    previous_value,
+    modify_tx,
+    valid_from,
+    valid_to,
+    inserted_ts,
+    modified_ts,
+    protocol_component_id
+FROM
+    latest_protocol_state;
+
+--- Create historical partitions, maybe this should not be done as part of the migration since it is quite slow....
+--- Unfortunately columns need to match to can't keep id column in old table to migrate data...
+ALTER TABLE protocol_state_old
+    DROP COLUMN id;
+
+CALL partman.partition_data_proc(p_parent_table := 'public.protocol_state', p_loop_count := 7, p_source_table := 'public.protocol_state_old', p_order := 'DESC');
 
 -- this constraint allows us to to upserts into this table.
-ALTER TABLE protocol_state_default ADD CONSTRAINT protocol_state_default_unique_pk UNIQUE (protocol_component_id, attribute_name);
-
-DROP TABLE protocol_state_old;
+ALTER TABLE protocol_state_default
+    ADD CONSTRAINT protocol_state_default_unique_pk UNIQUE (protocol_component_id, attribute_name);
 
 -- CONTRACT STATE
 ALTER TABLE contract_storage RENAME TO contract_storage_old;
 
 ALTER INDEX idx_contract_storage_account_id RENAME TO idx_contract_storage_account_id_old;
+
 ALTER INDEX idx_contract_storage_account_id_slot_valid_to RENAME TO idx_contract_storage_account_id_slot_valid_to_old;
+
 ALTER INDEX idx_contract_storage_valid_to RENAME TO idx_contract_storage_valid_to_old;
 
-UPDATE contract_storage_old SET valid_to = '262142-12-31T23:59:59.9999Z' WHERE valid_to IS NULL;
+UPDATE
+    contract_storage_old
+SET
+    valid_to = '262142-12-31T23:59:59.9999Z'
+WHERE
+    valid_to IS NULL;
 
-ALTER TABLE contract_storage_old ALTER COLUMN valid_to SET NOT NULL;
+ALTER TABLE contract_storage_old
+    ALTER COLUMN valid_to SET NOT NULL;
 
 CREATE TABLE contract_storage(
     slot bytea NOT NULL,
@@ -123,7 +156,8 @@ CREATE TABLE contract_storage(
     valid_to timestamptz NOT NULL,
     inserted_ts timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_ts timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
-) PARTITION BY RANGE (valid_to);
+)
+PARTITION BY RANGE (valid_to);
 
 CREATE INDEX idx_contract_storage_account_id ON public.contract_storage USING btree(account_id);
 
@@ -137,20 +171,49 @@ ALTER TABLE public.contract_storage
 ALTER TABLE public.contract_storage
     ADD CONSTRAINT contract_storage_modify_tx_fkey FOREIGN KEY (modify_tx) REFERENCES "transaction"(id) ON DELETE CASCADE;
 
-SELECT partman.create_parent(p_parent_table := 'public.contract_storage', p_control := 'valid_to', p_interval := '1 day', p_type := 'range', p_premake := 7, p_default_table := TRUE, p_automatic_maintenance := 'on');
+SELECT
+    partman.create_parent(p_parent_table := 'public.contract_storage', p_control := 'valid_to', p_interval := '1 day', p_type := 'range', p_premake := 7, p_default_table := TRUE, p_automatic_maintenance := 'on');
 
-CALL partman.partition_data_proc(p_parent_table := 'public.contract_storage', p_loop_count := 90, p_source_table := 'public.contract_storage_old', p_order := 'DESC');
+WITH latest_contract_storage AS (
+    DELETE FROM contract_storage_old
+    WHERE valid_to = '262142-12-31T23:59:59.9999Z'
+    RETURNING
+        *)
+INSERT INTO contract_storage
+SELECT
+    slot,
+    value,
+    previous_value,
+    account_id,
+    modify_tx,
+    ordinal,
+    valid_from,
+    valid_to,
+    inserted_ts,
+    modified_ts
+FROM
+    latest_contract_storage;
 
-ALTER TABLE contract_storage_default ADD CONSTRAINT contract_storage_default_unique_pk UNIQUE (account_id, slot);
+ALTER TABLE contract_storage_old
+    DROP COLUMN id;
 
-DROP TABLE contract_storage_old;
+CALL partman.partition_data_proc(p_parent_table := 'public.contract_storage', p_loop_count := 7, p_source_table := 'public.contract_storage_old', p_order := 'DESC');
+
+ALTER TABLE contract_storage_default
+    ADD CONSTRAINT contract_storage_default_unique_pk UNIQUE (account_id, slot);
 
 -- COMPONENT BALANCE
 ALTER TABLE component_balance RENAME TO component_balance_old;
 
-UPDATE component_balance_old SET valid_to = '262142-12-31T23:59:59.9999Z' WHERE valid_to IS NULL;
+UPDATE
+    component_balance_old
+SET
+    valid_to = '262142-12-31T23:59:59.9999Z'
+WHERE
+    valid_to IS NULL;
 
-ALTER TABLE component_balance_old ALTER COLUMN valid_to SET NOT NULL;
+ALTER TABLE component_balance_old
+    ALTER COLUMN valid_to SET NOT NULL;
 
 ALTER INDEX idx_component_balance_component_id_token_id_valid_to RENAME TO idx_component_balance_component_id_token_id_valid_to_old;
 
@@ -166,7 +229,8 @@ CREATE TABLE component_balance(
     inserted_ts timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     valid_from timestamptz NOT NULL,
     valid_to timestamptz NOT NULL
-) PARTITION BY RANGE (valid_to);
+)
+PARTITION BY RANGE (valid_to);
 
 CREATE INDEX idx_component_balance_component_id_token_id_valid_to ON public.component_balance USING btree(protocol_component_id, token_id, valid_to);
 
@@ -184,8 +248,30 @@ ALTER TABLE public.component_balance
 SELECT
     partman.create_parent(p_parent_table := 'public.component_balance', p_control := 'valid_to', p_interval := '1 day', p_type := 'range', p_premake := 7, p_default_table := TRUE, p_automatic_maintenance := 'on');
 
-CALL partman.partition_data_proc(p_parent_table := 'public.component_balance', p_loop_count := 90, p_source_table := 'public.component_balance_old', p_order := 'DESC');
+WITH latest_component_balance AS (
+    DELETE FROM component_balance_old
+    WHERE valid_to = '262142-12-31T23:59:59.9999Z'
+    RETURNING
+        *)
+INSERT INTO component_balance
+SELECT
+    token_id,
+    new_balance,
+    previous_value,
+    balance_float,
+    modify_tx,
+    protocol_component_id,
+    inserted_ts,
+    valid_from,
+    valid_to
+FROM
+    latest_component_balance;
 
-ALTER TABLE component_balance_default ADD CONSTRAINT component_balance_default_unique_pk UNIQUE (protocol_component_id, token_id);
+ALTER TABLE component_balance_old
+    DROP COLUMN id;
 
-DROP TABLE component_balance_old;
+CALL partman.partition_data_proc(p_parent_table := 'public.component_balance', p_loop_count := 7, p_source_table := 'public.component_balance_old', p_order := 'DESC');
+
+ALTER TABLE component_balance_default
+    ADD CONSTRAINT component_balance_default_unique_pk UNIQUE (protocol_component_id, token_id);
+
