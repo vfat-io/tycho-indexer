a5efeae46cb98be89907ca43067e438480246222
kayibal <alan@datarevenue.com>
2024-04-06 17:36:11 +0000

    Pin formatting and fmt checks in scripty

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 set -e 
 
-cargo +nightly fmt -- --check
-cargo +nightly clippy --all --all-features --all-targets -- -D warnings
+cargo +nightly-2024-04-03 fmt -- --check
+cargo +nightly-2024-04-03- clippy --all --all-features --all-targets -- -D warnings
 cargo nextest run --workspace --all-targets --all-features --bin tycho-indexer -E 'not test(serial_db)'
 cargo nextest run --workspace --all-targets --all-features --bin tycho-indexer -E 'test(serial_db)'
