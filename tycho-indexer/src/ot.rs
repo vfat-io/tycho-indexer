9a6ab0e7fd53c588d7b3d737fd209ed9e9189d4a
kayibal <alan@datarevenue.com>
2024-04-27 18:37:29 +0100

    feat: Add open telemetry tracing.

---
+++ tycho-indexer/src/ot.rs
@@ -0,0 +1,54 @@
+use anyhow::{Context, Result};
+use opentelemetry::{global, KeyValue};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{propagation::TraceContextPropagator, runtime, trace, Resource};
+use serde::Deserialize;
+use tracing::{error, Subscriber};
+use tracing_subscriber::{
+    layer::SubscriberExt, registry::LookupSpan, util::SubscriberInitExt, EnvFilter, Layer,
+};
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct TracingConfig {
+    pub service_name: String,
+    pub otlp_exporter_endpoint: String,
+}
+
+/// Initialize tracing: apply an `EnvFilter` using the `RUST_LOG` environment variable to define the
+/// log levels, add a formatter layer logging trace events as JSON and on OpenTelemetry layer
+/// exporting trace data.
+pub fn init_tracing(config: TracingConfig) -> Result<()> {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    global::set_error_handler(|error| error!(error = format!("{error:#}"), "otel error"))
+        .context("set error handler")?;
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env())
+        .with(otlp_layer(config)?)
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()
+        .context("initialize tracing subscriber")
+}
+
+/// Create an OTLP layer exporting tracing data.
+fn otlp_layer<S>(config: TracingConfig) -> Result<impl Layer<S>>
+where
+    S: Subscriber + for<'span> LookupSpan<'span>,
+{
+    let exporter = opentelemetry_otlp::new_exporter()
+        .tonic()
+        .with_endpoint(config.otlp_exporter_endpoint);
+
+    let trace_config = trace::config()
+        .with_resource(Resource::new(vec![KeyValue::new("service.name", config.service_name)]));
+
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(exporter)
+        .with_trace_config(trace_config)
+        .install_batch(runtime::Tokio)
+        .context("install tracer")?;
+
+    Ok(tracing_opentelemetry::layer().with_tracer(tracer))
+}
