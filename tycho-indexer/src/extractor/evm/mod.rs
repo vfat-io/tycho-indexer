59ad93958c5e5bb5daefdb445230fadf3aef139a
Thales <thales@datarevenue.com>
2024-05-20 22:35:10 -0300

    Add remaining BlockChangesResult methods

--- tycho-indexer/src/extractor/evm/mod.rs
+++ tycho-indexer/src/extractor/evm/mod.rs
@@ -8,6 +8,7 @@ use ethers::{
 };
 use serde::{Deserialize, Serialize};
 use std::{
+    any::Any,
     collections::{hash_map::Entry, HashMap, HashSet},
     sync::Arc,
 };
@@ -1689,6 +1690,83 @@ pub struct BlockChangesResult {
     pub account_updates: HashMap<H160, AccountUpdate>,
     pub component_balances: HashMap<ComponentId, HashMap<H160, ComponentBalance>>,
     pub component_tvl: HashMap<ComponentId, f64>,
+}
+
+impl BlockChangesResult {
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        extractor: &str,
+        chain: Chain,
+        block: Block,
+        finalized_block_height: u64,
+        revert: bool,
+        new_protocol_components: HashMap<ComponentId, ProtocolComponent>,
+        new_tokens: HashMap<Address, CurrencyToken>,
+        deleted_protocol_components: HashMap<ComponentId, ProtocolComponent>,
+        protocol_states: HashMap<ComponentId, ProtocolStateDelta>,
+        account_updates: HashMap<H160, AccountUpdate>,
+        component_balances: HashMap<ComponentId, HashMap<H160, ComponentBalance>>,
+    ) -> Self {
+        Self {
+            extractor: extractor.to_string(),
+            chain,
+            block,
+            finalized_block_height,
+            revert,
+            new_protocol_components,
+            new_tokens,
+            deleted_protocol_components,
+            protocol_states,
+            account_updates,
+            component_balances,
+            component_tvl: HashMap::new(),
+        }
+    }
+
+    #[allow(dead_code)]
+    fn is_empty(&self) -> bool {
+        self.new_protocol_components.is_empty() &&
+            self.deleted_protocol_components
+                .is_empty() &&
+            self.protocol_states.is_empty() &&
+            self.account_updates.is_empty() &&
+            self.component_balances.is_empty() &&
+            self.component_tvl.is_empty()
+    }
+}
+
+impl std::fmt::Display for BlockChangesResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "block_number: {}, extractor: {}", self.block.number, self.extractor)
+    }
+}
+
+#[typetag::serde]
+impl NormalisedMessage for BlockChangesResult {
+    fn source(&self) -> ExtractorIdentity {
+        ExtractorIdentity::new(self.chain, &self.extractor)
+    }
+
+    fn drop_state(&self) -> Arc<dyn NormalisedMessage> {
+        Arc::new(Self {
+            extractor: self.extractor.clone(),
+            chain: self.chain,
+            block: self.block,
+            finalized_block_height: self.finalized_block_height,
+            revert: self.revert,
+            account_updates: HashMap::new(),
+            protocol_states: HashMap::new(),
+            new_tokens: self.new_tokens.clone(),
+            new_protocol_components: self.new_protocol_components.clone(),
+            deleted_protocol_components: self.deleted_protocol_components.clone(),
+            component_balances: self.component_balances.clone(),
+            component_tvl: self.component_tvl.clone(),
+        })
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 #[derive(Debug, PartialEq, Default, Clone)]
