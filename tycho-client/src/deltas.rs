f4d2c7898379813f96ac65fd94837907cf9ccee9
Louise Poole <louise@datarevenue.com>
2024-05-31 10:19:59 +0200

    fix[client]: set retry wait to 0.5 sec

--- tycho-client/src/deltas.rs
+++ tycho-client/src/deltas.rs
@@ -587,7 +587,7 @@ impl DeltasClient for WsDeltasClient {
                             *guard = None;
 
                             warn!(?e, "Failed to connect to WebSocket server; Reconnecting");
-                            sleep(Duration::from_secs(1)).await;
+                            sleep(Duration::from_millis(500)).await;
 
                             continue 'retry;
                         }
