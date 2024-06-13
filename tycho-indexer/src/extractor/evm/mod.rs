a5c85354b9398258dc4646a7649e5afda31481b6
Florian Pellissier <111426680+flopell@users.noreply.github.com>
2024-06-13 14:45:44 +0200

    refactor: only shift block ts on Arbitrum

--- tycho-indexer/src/extractor/evm/mod.rs
+++ tycho-indexer/src/extractor/evm/mod.rs
@@ -612,22 +612,26 @@ impl BlockScoped for BlockContractChanges {
 impl Block {
     /// Parses block from tychos protobuf block message
     pub fn try_from_message(msg: substreams::Block, chain: Chain) -> Result<Self, ExtractionError> {
+        let ts_nano = match chain {
+            // For blockchains with subsecond block times, like Arbitrum, timestamps aren't precise
+            // enough to distinguish between two blocks accurately. To maintain accurate ordering,
+            // we adjust timestamps by appending part of the current block number as microseconds.
+            Chain::Arbitrum => (msg.number as u32 % 1000) * 1000,
+            _ => 0,
+        };
+
         Ok(Self {
             chain,
             number: msg.number,
             hash: pad_and_parse_32bytes(&msg.hash).map_err(ExtractionError::DecodeError)?,
             parent_hash: pad_and_parse_32bytes(&msg.parent_hash)
                 .map_err(ExtractionError::DecodeError)?,
-            // For blockchains with subsecond block times, like Arbitrum, timestamps aren't precise
-            // enough to distinguish between two blocks accurately. To maintain accurate ordering,
-            // we adjust timestamps by appending part of the current block number as microseconds.
-            ts: NaiveDateTime::from_timestamp_opt(msg.ts as i64, (msg.number as u32 % 1000) * 1000)
-                .ok_or_else(|| {
-                    ExtractionError::DecodeError(format!(
-                        "Failed to convert timestamp {} to datetime!",
-                        msg.ts
-                    ))
-                })?,
+            ts: NaiveDateTime::from_timestamp_opt(msg.ts as i64, ts_nano).ok_or_else(|| {
+                ExtractionError::DecodeError(format!(
+                    "Failed to convert timestamp {} to datetime!",
+                    msg.ts
+                ))
+            })?,
         })
     }
 }
