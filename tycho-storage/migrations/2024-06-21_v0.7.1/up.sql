ef92989ee682003a6add6900a397f11cb0e7a9da
Louise Poole <louise@datarevenue.com>
2024-09-06 18:42:33 +0200

    fix: add chain awareness to extraction state block migration

--- tycho-storage/migrations/2024-06-21_v0.7.1/up.sql
+++ tycho-storage/migrations/2024-06-21_v0.7.1/up.sql
@@ -2,15 +2,18 @@
 ALTER TABLE extraction_state
 ADD COLUMN "block_id" bigint REFERENCES "block"(id);
 
--- populate existing extraction_state with oldest block_id
-WITH oldest_block AS (
-    SELECT id
-    FROM "block"
-    ORDER BY id ASC
-    LIMIT 1
+-- Populate existing extraction_state with the oldest block_id for its chain
+WITH oldest_blocks AS (
+    SELECT b.chain_id, MIN(b.id) AS oldest_block_id
+    FROM "block" b
+    GROUP BY b.chain_id
 )
-UPDATE extraction_state
-SET block_id = (SELECT id FROM oldest_block);
+UPDATE extraction_state es
+SET block_id = (
+    SELECT ob.oldest_block_id
+    FROM oldest_blocks ob
+    WHERE es.chain_id = ob.chain_id
+);
 
 -- add NOT NULL constraint
 ALTER TABLE extraction_state ALTER COLUMN block_id SET NOT NULL;
