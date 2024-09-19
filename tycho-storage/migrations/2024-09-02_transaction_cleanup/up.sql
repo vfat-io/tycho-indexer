ceb5376ed4da853ae80f00410e7c442f59a1cd4d
Louise Poole <louise@datarevenue.com>
2024-09-19 12:53:41 +0200

    fix: skip batching on search phase

--- tycho-storage/migrations/2024-09-02_transaction_cleanup/up.sql
+++ tycho-storage/migrations/2024-09-02_transaction_cleanup/up.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION clean_transaction_table() RETURNS void AS $$
 DECLARE
-    batch_size INT := 1000;
+    batch_size INT := 10;
     rows_deleted INT;
     offset_val INT := 0;
     orphaned_count INT;  -- Variable to store count of orphaned transactions
@@ -10,92 +10,78 @@ BEGIN
         id BIGINT PRIMARY KEY
     );
 
-    -- Create temp_batch_transaction_ids once and truncate it after each batch instead of dropping and creating.
-    CREATE TEMP TABLE temp_batch_transaction_ids (
-        id BIGINT PRIMARY KEY
+    RAISE NOTICE 'Searching for orphaned transactions';
+
+    INSERT INTO temp_transaction_ids (id)
+    SELECT t2.id
+    FROM "transaction" t2;
+
+    DELETE FROM temp_transaction_ids
+    WHERE id IN (
+        SELECT t2.id
+        FROM temp_transaction_ids t2
+        JOIN contract_code cc ON cc.modify_tx = t2.id
     );
 
-    LOOP
-        TRUNCATE temp_batch_transaction_ids;
+	RAISE NOTICE 'Scanned contract_code';
 
-        RAISE NOTICE 'Processing batch with OFFSET %', offset_val;
-
-        INSERT INTO temp_batch_transaction_ids (id)
+    DELETE FROM temp_transaction_ids
+    WHERE id IN (
         SELECT t2.id
-        FROM "transaction" t2
-        ORDER BY t2.id
-        LIMIT batch_size OFFSET offset_val;
+        FROM temp_transaction_ids t2
+        JOIN protocol_component pc ON pc.creation_tx = t2.id OR pc.deletion_tx = t2.id
+    );
 
-        -- Check if there are no more transactions to process
-        IF NOT FOUND THEN
-            -- Calculate the number of orphaned transactions
-            SELECT COUNT(*) INTO orphaned_count FROM temp_transaction_ids;
-            RAISE NOTICE 'SEARCH COMPLETE. Found % total orphaned transactions', orphaned_count;
-            EXIT;
-        END IF;
+	RAISE NOTICE 'Scanned protocol_component';
 
-        offset_val := offset_val + batch_size;
+    DELETE FROM temp_transaction_ids
+    WHERE id IN (
+        SELECT t2.id
+        FROM temp_transaction_ids t2
+        JOIN account a ON a.creation_tx = t2.id OR a.deletion_tx = t2.id
+    );
 
-        DELETE FROM temp_batch_transaction_ids
-        WHERE id IN (
-            SELECT t2.id
-            FROM temp_batch_transaction_ids t2
-            JOIN contract_code cc ON cc.modify_tx = t2.id
-        );
+	RAISE NOTICE 'Scanned account';
 
-        DELETE FROM temp_batch_transaction_ids
-        WHERE id IN (
-            SELECT t2.id
-            FROM temp_batch_transaction_ids t2
-            JOIN protocol_component pc ON pc.creation_tx = t2.id OR pc.deletion_tx = t2.id
-        );
+    DELETE FROM temp_transaction_ids
+    WHERE id IN (
+        SELECT t2.id
+        FROM temp_transaction_ids t2
+        JOIN account_balance ab ON ab.modify_tx = t2.id
+    );
 
-        DELETE FROM temp_batch_transaction_ids
-        WHERE id IN (
-            SELECT t2.id
-            FROM temp_batch_transaction_ids t2
-            JOIN account a ON a.creation_tx = t2.id OR a.deletion_tx = t2.id
-        );
+	RAISE NOTICE 'Scanned account_balance';
 
-        DELETE FROM temp_batch_transaction_ids
-        WHERE id IN (
-            SELECT t2.id
-            FROM temp_batch_transaction_ids t2
-            JOIN account_balance ab ON ab.modify_tx = t2.id
-        );
+    DELETE FROM temp_transaction_ids
+    WHERE id IN (
+        SELECT t2.id
+        FROM temp_transaction_ids t2
+        JOIN component_balance cb ON cb.modify_tx = t2.id
+    );
 
-        DELETE FROM temp_batch_transaction_ids
-        WHERE id IN (
-            SELECT t2.id
-            FROM temp_batch_transaction_ids t2
-            JOIN component_balance cb ON cb.modify_tx = t2.id
-        );
+	RAISE NOTICE 'Scanned component_balance';
 
-        DELETE FROM temp_batch_transaction_ids
-        WHERE id IN (
-            SELECT t2.id
-            FROM temp_batch_transaction_ids t2
-            JOIN protocol_state ps ON ps.modify_tx = t2.id
-        );
+    DELETE FROM temp_transaction_ids
+    WHERE id IN (
+        SELECT t2.id
+        FROM temp_transaction_ids t2
+        JOIN protocol_state ps ON ps.modify_tx = t2.id
+    );
 
-        DELETE FROM temp_batch_transaction_ids
-        WHERE id IN (
-            SELECT t2.id
-            FROM temp_batch_transaction_ids t2
-            JOIN contract_storage cs ON cs.modify_tx = t2.id
-        );
+	RAISE NOTICE 'Scanned protocol_state';
 
-        SELECT COUNT(*) INTO orphaned_count FROM temp_batch_transaction_ids;
-        RAISE NOTICE 'Found % orphaned transactions', orphaned_count;
+    DELETE FROM temp_transaction_ids
+    WHERE id IN (
+        SELECT t2.id
+        FROM temp_transaction_ids t2
+        JOIN contract_storage cs ON cs.modify_tx = t2.id
+    );
 
-        INSERT INTO temp_transaction_ids (id)
-        SELECT id
-        FROM temp_batch_transaction_ids;
+	RAISE NOTICE 'Scanned contract_storage';
 
-        -- Pause between batches to reduce load
-        PERFORM pg_sleep(1);
+    SELECT COUNT(*) INTO orphaned_count FROM temp_transaction_ids;
+    RAISE NOTICE 'Found % orphaned transactions', orphaned_count;
 
-    END LOOP;
 
     -- Phase 2: Delete the collected transaction IDs in batches
     LOOP
@@ -131,7 +117,6 @@ BEGIN
 
     -- Drop the temporary tables when done
     DROP TABLE IF EXISTS temp_transaction_ids;
-    DROP TABLE IF EXISTS temp_batch_transaction_ids;
 
 END;
 $$ LANGUAGE plpgsql;
