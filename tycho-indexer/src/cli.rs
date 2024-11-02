f7422c7d2f9b475f6e65a8c0d2c3b18096b7d810
zizou <111426680+flopell@users.noreply.github.com>
2024-11-02 10:24:32 +0700

    refactor(cli): add default value for s3.
    This is for backward compatibility

--- tycho-indexer/src/cli.rs
+++ tycho-indexer/src/cli.rs
@@ -50,7 +50,8 @@ pub struct GlobalArgs {
     pub database_url: String,
 
     /// Name of the s3 bucket used to retrieve spkgs
-    #[clap(env = "TYCHO_S3_BUCKET", long)]
+    #[clap(env = "TYCHO_S3_BUCKET", long, default_value = "repo.propellerheads")]
+    //Default is for backward compatibility but needs to be removed later
     pub s3_bucket: Option<String>,
 
     /// Substreams API endpoint
@@ -214,7 +215,7 @@ mod cli_tests {
             global_args: GlobalArgs {
                 endpoint_url: "http://example.com".to_string(),
                 database_url: "my_db".to_string(),
-                s3_bucket: None,
+                s3_bucket: Some("repo.propellerheads".to_string()),
                 server_ip: "0.0.0.0".to_string(),
                 server_port: 4242,
                 server_version_prefix: "v1".to_string(),
@@ -260,7 +261,7 @@ mod cli_tests {
             global_args: GlobalArgs {
                 endpoint_url: "http://example.com".to_string(),
                 database_url: "my_db".to_string(),
-                s3_bucket: None,
+                s3_bucket: Some("repo.propellerheads".to_string()),
                 server_ip: "0.0.0.0".to_string(),
                 server_port: 4242,
                 server_version_prefix: "v1".to_string(),
