1bbdffe2398becd19fc5e301c673831128dd3b3c
Louise Poole <louise@datarevenue.com>
2025-03-05 11:46:15 +0200

    refactor: remove duplicated tycho proto message files
    These files are on the protocol-sdk repo and should be imported from there.

--- tycho-indexer/src/pb/pb.rs
+++ tycho-indexer/src/pb/pb.rs
@@ -34,12 +34,3 @@ pub mod sf {
         }
     }
 }
-pub mod tycho {
-    pub mod evm {
-        // @@protoc_insertion_point(attribute:tycho.evm.v1)
-        pub mod v1 {
-            include!("tycho.evm.v1.rs");
-            // @@protoc_insertion_point(tycho.evm.v1)
-        }
-    }
-}
